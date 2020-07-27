d06a1f2ccbee0840f0a92cfbf53e9831b3973ee5
dimasyahrul <dimascikoci@gmail.com>
2020-07-27 22:17:08 +0700

    dimasyahrul

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Menu from '../components/menu';
+export default function Home() {
+return (
+<div>
+<Head>
+<title>Home</title>
+</Head>
+<Menu />
+<div>
+<h1>WELCOME TO WEB PSTORE</h1>
+<p>Pstore adalah salah satu perusahaan jual beli online di bidang teknologi, salah satu produknya adalah handphone dan alat komunikasi lainya. 
+
+Handphone saat ini sudah menjadi salah satu barang yang tak bisa lepas dari tangan. Entah untuk menunjang pekerjaan atau hanya sekadar bergaya, orang merasa harus memiliki handphone di era serba digital ini. Masyarakat bisa leluasa memilih tipe handphone yang dibutuhkannya dengan harga yang semakin murah dan program yang semakin canggih. 
+
+Selain itu langkah penjual belinya sangat praktis dan mudah sehingga customer tidak repot pergi ketoko, lantaran di dalam program pstore ini melayani pembelian secara cash of delivery. 
+
+
+.</p>
+</div>
+</div>
+);
+}
---
+++ pages/tentang.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Menu from '../components/menu';
+export default function Tentang() {
+return (
+<div>
+<Head>
+<title>Tentang Kami</title>
+</Head>
+<Menu />
+<div>
+<h1>Tentang Kami</h1>
+<h5>Mahasiswa Stikom Banyuwangi</h5>
+<p>Nama  : DIMAS SYAHRUL ASYIRI</p>
+<P>Tempat, Tgl Lahir : Banyuwangi, 14 JANUARI 2000</P>
+<p>Jenis Kelamin : Laki-Laki</p>
+<p>Agama : Islam</p>
+<p>Alamat : Jl.PANTAI BOMO - BLIMBINGSARI</p>
+<p>Kelas : SM4.1 (S1 MALAM)</p>
+<p>Email : dimascikoci@gmail.com</p>
+</div>
+</div>
+);
+}
